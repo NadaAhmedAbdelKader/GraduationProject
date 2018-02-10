bd99db2e4682eca41b13da010aa8939051a50b5e
AlaaDabour7 <Alaa.dabbour95@gmail.com>
2018-02-10 17:07:09 +0200

    Merging

--- app/src/main/java/com/switchak/switchak/CostFragment.java
+++ app/src/main/java/com/switchak/switchak/CostFragment.java
@@ -1,12 +1,19 @@
 package com.switchak.switchak;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.RelativeSizeSpan;
+import android.text.style.StyleSpan;
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -14,6 +21,7 @@ import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -87,13 +95,43 @@ public class CostFragment extends Fragment {
         entries.add(new PieEntry(1, (float) 12.5));
         entries.add(new PieEntry(2, (float) 17.5));
         entries.add(new PieEntry(3, (float) 10));
+        entries.add(new PieEntry(4, (float) 20));
+
 
         PieDataSet dataSet = new PieDataSet(entries, "Label");
+
+        final int[] MY_COLORS = {Color.rgb(192,192,0), Color.rgb(255,0,0), Color.rgb(0,0,192),Color.rgb(0,200,0) };
+        ArrayList<Integer> colors = new ArrayList<Integer>();
+
+        for(int c: MY_COLORS) colors.add(c);
+
+        dataSet.setColors(colors);
+        dataSet.setValueTextSize(15f);
+
 
         PieData pieData = new PieData(dataSet);
         pieChart.setData(pieData);
         pieChart.invalidate(); // refresh
+        pieChart.setUsePercentValues(true);
 
+        pieChart.setDrawHoleEnabled(true);
+        pieChart.setHoleColor(Color.WHITE);
+
+        pieChart.setTransparentCircleColor(Color.WHITE);
+        pieChart.setTransparentCircleAlpha(110);
+
+        pieChart.setHoleRadius(60f);
+        pieChart.setTransparentCircleRadius(55f);
+
+        pieChart.setRotationAngle(0);
+        // enable rotation of the chart by touch
+        pieChart.setRotationEnabled(false);
+        pieChart.setHighlightPerTapEnabled(true);
+
+
+
+
+        pieChart.setEntryLabelColor(Color.WHITE);
 
         // Inflate the layout for this fragment
         return rootView;
--- app/src/main/java/com/switchak/switchak/HistoryFragment.java
+++ app/src/main/java/com/switchak/switchak/HistoryFragment.java
@@ -1,8 +1,10 @@
 package com.switchak.switchak;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -10,11 +12,20 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.ChartData;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.ChildEventListener;
@@ -25,8 +36,12 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import static android.content.ContentValues.TAG;
 
@@ -145,14 +160,103 @@ public class HistoryFragment extends Fragment {
         List<Entry> entries = new ArrayList<>();
 
         entries.add(new Entry(1, (float) 12.5));
-        entries.add(new Entry(2, (float) 17.5));
-        entries.add(new Entry(3, (float) 10));
+        entries.add(new Entry(6, (float) 17.5));
+        entries.add(new Entry(16, (float) 10));
+        entries.add(new Entry(20, (float) 15));
+
+        List< Timestamp > Time = new ArrayList<>();
+
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.HOUR_OF_DAY, 0); // ! clear would not reset the hour of day !
+        cal.clear(Calendar.MINUTE);
+        cal.clear(Calendar.SECOND);
+        cal.clear(Calendar.MILLISECOND);
+
+        // get start of the month
+        cal.set(Calendar.DAY_OF_MONTH, 1);
+        System.out.println("Start of the month:       " + cal.getTime());
+        System.out.println("... in milliseconds:      " + cal.getTimeInMillis());
+
+        // get start of the next month
+        cal.add(Calendar.MONTH, 1);
+        System.out.println("Start of the next month:  " + cal.getTime());
+        System.out.println("... in milliseconds:      " + cal.getTimeInMillis());
+
+        Calendar now = Calendar.getInstance();
+        Date date = new Date();
+        now.setTime(date);
+
+        cal.compareTo(now);
+
+        XAxis xAxis = chart.getXAxis();
+
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM_INSIDE);
+        xAxis.setAvoidFirstLastClipping(true);
+
+        xAxis.setDrawAxisLine(true);
+        xAxis.setEnabled(true);
+        xAxis.setTextSize(10f);
+        xAxis.setTextColor(Color.WHITE);
+        xAxis.setDrawAxisLine(true);
+        xAxis.setDrawGridLines(true);
+        xAxis.setTextColor(Color.rgb(255, 100, 100));
+        xAxis.setCenterAxisLabels(true);
+        xAxis.setGranularity(1f); // one hour
+
+        xAxis.setAxisMinimum(0);
+        xAxis.setAxisMaximum(31);
+
+
+        xAxis.setValueFormatter(new IAxisValueFormatter() {
+
+                                    private SimpleDateFormat mFormat = new SimpleDateFormat("dd  ");
+
+
+                                    @Override
+                                  public String getFormattedValue(float value, AxisBase axis) {
+
+                                        long millis = TimeUnit.DAYS.toMillis((long) value);
+                                        return mFormat.format(new Date(millis));
+                                    }
+        });
+
+
+        YAxis yAxis = chart.getAxisLeft();
+        yAxis.setPosition(YAxis.YAxisLabelPosition.INSIDE_CHART);
+       // leftAxis.setTextColor(ColorTemplate.getHoloBlue());
+
+        yAxis.setDrawGridLines(true);
+        yAxis.setTextColor(Color.rgb(255, 192, 56));
+        YAxis rightAxis = chart.getAxisRight();
+        yAxis.setDrawZeroLine(true);
+        rightAxis.setEnabled(false);
+
+        yAxis.setAxisMinimum(0);
+        chart.getAxisLeft().setStartAtZero(true);
+
 
         LineDataSet dataSet = new LineDataSet(entries, "Label");
+        dataSet.setAxisDependency(YAxis.AxisDependency.LEFT);
 
         LineData lineData = new LineData(dataSet);
         chart.setData(lineData);
         chart.invalidate();
+
+        // enable scaling and dragging
+        chart.setDragEnabled(true);
+        chart.setScaleEnabled(true);
+
+
+        LineDataSet dataset = new LineDataSet(entries, "");
+        dataset.isDrawCirclesEnabled();
+        dataset.setCircleRadius(0f);
+        dataset.setDrawFilled(true);
+        dataset.setFillColor(Color.rgb(0, 0, 0));
+        dataset.setLineWidth(0.2f);
+        dataset.setValueTextSize(0f);
+
+
+
 
         // Inflate the layout for this fragment
         return rootView;
@@ -195,5 +299,8 @@ public class HistoryFragment extends Fragment {
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
+    }
+
+    private class AxisValueFormatter {
     }
 }
